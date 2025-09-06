de04d6500dc007f12a450879fce7764a99ff16f3
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-09-06 10:17:29 -0300

    first test ensuring the condominium api is returning a json on #index

--- condo-api/spec/requests/condominium_spec.rb
+++ condo-api/spec/requests/condominium_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "Condominia", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    before do
+      get condominia_url
+    end
+
+    it "returns a list of condominium" do
+      json_response = JSON.parse(response.body)
+
+      expect(json_response).to be_an(Array)
+    end
   end
 end
