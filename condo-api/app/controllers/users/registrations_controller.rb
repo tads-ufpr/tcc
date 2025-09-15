d5ed5d3c57acf256849b5b4b7ae79ac0326f445e
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-09-15 11:50:47 -0300

    add revocation strategy by jti

---
+++ condo-api/app/controllers/users/registrations_controller.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  include RackSessionsFix
+  respond_to :json
+
+  private
+  def respond_with(current_user, _opts = {})
+    if resource.persisted?
+      render json: {
+        status: { code: 200, message: "Signed up successfully" },
+        data: UserSerializer.new(current_user).serializable_hash[:data][:attributes]
+      }
+    else
+      render json: {
+        status: {
+          status: :unprocessable_entity,
+          message: "
+            User couldn't be created successfully.
+            #{current_user.errors.full_messages.to_sentence}
+          "
+        }
+      }
+    end
+  end
+end
