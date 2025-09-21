993413e01273585e5457aece1f5e84f33fe23832
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-09-21 11:46:29 -0300

    fix user's factory adding password to it also renaming email_address to email

--- condo-api/spec/factories/users.rb
+++ condo-api/spec/factories/users.rb
@@ -1,9 +1,10 @@
 FactoryBot.define do
   factory :user do
-    sequence(:email_address) { |n| "email#{n}@condo.com.br" }
+    sequence(:email) { |n| "email#{n}@condo.com.br" }
     document { Faker::IdNumber.brazilian_citizen_number }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     birthdate { Faker::Date.birthday(min_age: 18, max_age: 80) }
+    password { "pokpok" }
   end
 end
