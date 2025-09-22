ba2310ab6672dea1e1d8c03250432c6eb2267762
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-09-21 22:40:59 -0300

    bring current_user definition to application_controller in order to define it's logic though jwt and use as Cancancan resource

--- condo-api/app/controllers/application_controller.rb
+++ condo-api/app/controllers/application_controller.rb
@@ -1,3 +1,32 @@
 class ApplicationController < ActionController::API
-  before_action :authenticate_user!
+  include CanCan::ControllerAdditions
+
+  rescue_from CanCan::AccessDenied do |exception|
+    render json: { error: exception.message }, status: :unauthorized
+  end
+
+  protected
+
+  def current_user
+    @current_user ||= find_current_user_from_jwt
+  end
+
+  private
+  def find_current_user_from_jwt
+    token = request.headers["Authorization"]&.split(" ")&.last
+
+    return nil unless token
+
+    begin
+      payload = JWT.decode(
+        token,
+        Rails.application.credentials.devise_jwt_secret_key!
+      ).first
+
+      User.find_by(id: payload["sub"])
+
+    rescue JWT::DecodeError
+      nil
+    end
+  end
 end
