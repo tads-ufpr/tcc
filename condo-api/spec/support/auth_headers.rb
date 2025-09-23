bc8e638513d5f53e95205100442aea51533204c3
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-09-23 19:24:02 -0300

    fix miss usage of wrong secret in order to encode e decode

--- condo-api/spec/support/auth_headers.rb
+++ condo-api/spec/support/auth_headers.rb
@@ -4,9 +4,8 @@ require 'jwt'
 RSpec.shared_context 'auth_headers', shared_context: :metadata do
   def authenticated_headers_for(user)
     payload = { user_id: user.id }
-    secret_key = Rails.application.credentials.secret_key_base
+    secret_key = Rails.application.credentials.devise_jwt_secret_key
     token = JWT.encode(payload, secret_key, 'HS256')
-    sign_in(user)
     { 'Authorization' => "Bearer #{token}" }
   end
 end
