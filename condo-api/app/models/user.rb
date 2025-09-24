a77aa9773d70cfbc0d13efa4fd6054479216861a
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-09-23 23:22:17 -0300

    add document alias as cpf, also added logic to handle user's name into a single string definition

--- condo-api/app/models/user.rb
+++ condo-api/app/models/user.rb
@@ -7,4 +7,19 @@ class User < ApplicationRecord
 
   has_many :employees
   has_many :condominia, through: :employees
+
+  alias_attribute :cpf, :document
+
+  def name
+    [first_name, last_name].compact.join(" ")
+  end
+
+  def name=(full_name)
+    sanitize_name = full_name.to_s.strip
+
+    parts = sanitize_name.split(" ", 2)
+
+    self.first_name = parts[0]
+    self.last_name = parts[1]
+  end
 end
