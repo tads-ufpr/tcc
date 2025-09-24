21a0d7387f054a659ad51273d3b08d2e06f4174d
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-09-24 15:46:29 -0300

    add relation between condominium and apartment

--- condo-api/app/models/condominium.rb
+++ condo-api/app/models/condominium.rb
@@ -2,6 +2,8 @@ class Condominium < ApplicationRecord
   has_many :employees, dependent: :destroy
   has_many :users, through: :employees
 
+  has_many :apartments, dependent: :destroy
+
   validates :city, :state, :address, presence: true
   validates :neighborhood, :zipcode, :number, presence: true
   validates :name, presence: true, uniqueness: { scope: :city }
