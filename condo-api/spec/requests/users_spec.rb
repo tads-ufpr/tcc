4525f251de116997b27a43c0ee27f5284675a8f7
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-10-20 15:35:58 -0300

    created tests to ensure properly validation on registration

---
+++ condo-api/spec/requests/users_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+  include_context "json_requests"
+  include_context "auth_headers"
+
+  let(:user_params) { { user: params } }
+  let(:params) { {
+      name: "Test Tester",
+      email: "test@test.com",
+      birthdate: "15/12/2000",
+      password: "test123",
+      password_confirmation: "test123",
+      document: Faker::IdNumber.brazilian_citizen_number
+  }}
+
+  describe "POST /users" do
+    describe "with missing parameters" do
+      before do |test|
+        params.delete(test.metadata[:missing].to_sym)
+
+        post users_url, params: user_params.to_json, headers: json_headers
+      end
+
+      it "deny creating without password", missing: "password" do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it "deny creating without email", missing: "email" do
+        response_json = JSON.parse(response.body)
+
+        expect(response_json["errors"]).to have_key("email")
+      end
+
+      it "deny crearting without document", missing: "document" do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+end
