a7693e38990d0be2162cf4d35b5894dec8c24966
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-10-20 16:06:44 -0300

    returning the Authorization header properly configured when created a new user

--- condo-api/app/controllers/users_controller.rb
+++ condo-api/app/controllers/users_controller.rb
@@ -8,6 +8,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      token = Warden::JWTAuth::UserEncoder.new.call(@user, :user, nil).first
+      response.headers["Authorization"] = "Bearer #{token}"
+
       render :created, status: :created
     else
       render json: { errors: @user.errors }, status: :unprocessable_entity
