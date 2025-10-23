4592e8428fe619e9dd18d579d872bb8983cd5fcd
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-10-23 14:17:17 -0300

    only employee is allowed to create a notice and residents are allowed to read it

--- condo-api/app/models/ability.rb
+++ condo-api/app/models/ability.rb
@@ -10,8 +10,13 @@ class Ability
     return unless user.present?
 
     can :create, Condominium
-    can :manage, Condominium, employees: { user_id: user.id, role: "admin" }
+    can :manage, Condominium, employees: { user_id: user.id, role: 'admin' }
 
     can :read, Apartment, condominium: { id: user.related_condominia_ids }
+
+    # Allowing the employee to manage any Notice from the Conodminium where he is employeed
+    can :manage, Notice, apartment: { condominium_id: user.employees.pluck(:condominium_id) }
+    # User can read Notices from any of his apartments
+    can :read, Notice, apartment: { id: user.apartments.pluck(:id) }
   end
 end
