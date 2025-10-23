0991ce5a15ce638391a4875e7c7af6ecb076b161
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-10-23 14:06:39 -0300

    add tests ensuring the new nested routes to condominia or apartments over notices

--- condo-api/spec/routing/notices_routing_spec.rb
+++ condo-api/spec/routing/notices_routing_spec.rb
@@ -2,29 +2,32 @@ require "rails_helper"
 
 RSpec.describe NoticesController, type: :routing do
   describe "routing" do
-    it "routes to #index" do
-      expect(get: "/notices").to route_to("notices#index")
+    describe "nested /condominia routes" do
+      it "routes to #index" do
+        expect(get: "/condominia/1/notices").to route_to("notices#index", condominium_id: '1')
+      end
     end
 
-    it "routes to #show" do
-      expect(get: "/notices/1").to route_to("notices#show", id: "1")
-    end
+    describe "nested /apartment routes" do
+      it "routes to #index" do
+        expect(get: "/apartments/1/notices").to route_to("notices#index", apartment_id: '1')
+      end
 
-
-    it "routes to #create" do
-      expect(post: "/notices").to route_to("notices#create")
+      it "routes to #create" do
+        expect(post: "/apartments/1/notices").to route_to("notices#create", apartment_id: '1')
+      end
     end
 
     it "routes to #update via PUT" do
-      expect(put: "/notices/1").to route_to("notices#update", id: "1")
+      expect(put: "/notices/1").to route_to("notices#update", id: '1')
     end
 
     it "routes to #update via PATCH" do
-      expect(patch: "/notices/1").to route_to("notices#update", id: "1")
+      expect(patch: "/notices/1").to route_to("notices#update", id: '1')
     end
 
     it "routes to #destroy" do
-      expect(delete: "/notices/1").to route_to("notices#destroy", id: "1")
+      expect(delete: "/notices/1").to route_to("notices#destroy", id: '1')
     end
   end
 end
