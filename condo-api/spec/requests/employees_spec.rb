963de28a184e7f5ad944f0bb50ce6e0559672019
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-10-26 10:19:04 -0300

    wip - employees_controller testing scenarios

--- condo-api/spec/requests/employees_spec.rb
+++ condo-api/spec/requests/employees_spec.rb
@@ -4,16 +4,17 @@ RSpec.describe "Employees", type: :request do
   include_context 'json_requests'
   include_context 'auth_headers'
 
-  let(:condo) {
+  let!(:condo) {
     create(:condominium, :with_staff, :with_residents, residents_count: 5)
   }
+  let(:condominium_id) { condo.id }
 
   describe "GET /condominia/:id/employees" do
     before do |test|
       headers = json_headers
       headers = headers.merge(authenticated_headers_for(user)) if test.metadata[:auth]
 
-      get condominium_employees_url(condo.id), headers:
+      get condominium_employees_url(condominium_id), headers:
     end
 
     describe "unauthenticated" do
@@ -32,7 +33,7 @@ RSpec.describe "Employees", type: :request do
 
     describe "with Condominium's unrelated :employee", :auth do
       let(:user) do
-        condo_2 = create(:condomininium, :with_staff)
+        condo_2 = create(:condominium, :with_staff)
         condo_2.employees.first.user
       end
 
@@ -65,6 +66,63 @@ RSpec.describe "Employees", type: :request do
 
       it "allows the request" do
         expect(response).to have_http_status(:success)
+      end
+    end
+  end
+
+  describe "POST /condominia/:condominium_id/employees" do
+    let(:params) {
+      attributes_for(:employee,
+                     condominium_id: condominium_id,
+                     user_id: condo.residents.first.user.id
+      ).to_json
+    }
+
+    before do |test|
+      headers = json_headers
+      headers = headers.merge(authenticated_headers_for(user)) if test.metadata[:auth]
+
+      post condominium_employees_url(condominium_id), params:, headers:
+    end
+
+    describe "unauthenticated" do
+      it "deny access" do
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    describe "when :resident", :auth do
+      let(:user) { condo.residents.first.user }
+
+      it "deny access" do
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
+
+    describe "when :employee from unrelated Condominium", :auth do
+      let(:user) do
+        condo_2 = create(:condominium, :with_staff)
+        condo_2.employees.first.user
+      end
+
+      it "deny access" do
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
+
+    describe "when :employee", :auth do
+      let(:user) { condo.employees.first.user }
+
+      context "with empty" do
+        let(:params) { {}.to_json }
+
+        it "invalidate the request" do
+          expect(response).to have_http_status(:bad_request)
+        end
+      end
+
+      it "allows the request" do
+        expect(response).to have_http_status(:created)
       end
     end
   end
