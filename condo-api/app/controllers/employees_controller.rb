e476738460652225f1d2f87731506a9043186748
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-10-26 10:17:56 -0300

    wip - employees_controller crud interface

--- condo-api/app/controllers/employees_controller.rb
+++ condo-api/app/controllers/employees_controller.rb
@@ -1,18 +1,39 @@
 class EmployeesController < ApplicationController
+  load_and_authorize_resource :condominium, only: [:create, :index]
+  load_and_authorize_resource :employee, through: :condominium, only: [:index, :create]
   load_and_authorize_resource :employee, only: [:show, :update, :destroy]
 
   def index
+    render json: @employees
   end
 
   def show
+    render json: @employee
   end
 
   def create
+    if @employee.save
+      render json: @employee, status: :created
+    else
+      render json: @employee.errors, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @employee.update(employee_params)
+      render json: @employee
+    else
+      render json: @employee.errors, status: :unprocessable_entity
+    end
   end
 
-  def delete
+  def destroy
+    @employee.destroy
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:role, :user_id, :description)
   end
 end
