b83ecacbebdc15180181b5f7285c49cb36deb997
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-10-27 15:10:08 -0300

    add apartment's owner persistency on resident's table

--- condo-api/app/models/resident.rb
+++ condo-api/app/models/resident.rb
@@ -3,4 +3,10 @@ class Resident < ApplicationRecord
   belongs_to :apartment
 
   validates :user_id, uniqueness: { scope: :apartment_id }
+  validate :first_resident?, on: :create
+
+  private
+  def first_resident?
+    self.owner = self.apartment.residents.where(owner: true).count == 0
+  end
 end
