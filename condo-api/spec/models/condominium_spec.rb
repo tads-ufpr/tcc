711668fe726ea301043063d4aba18101a9939c28
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-11-04 16:37:34 -0300

    fix outdated shoulda_matchers

--- condo-api/spec/models/condominium_spec.rb
+++ condo-api/spec/models/condominium_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Condominium, type: :model do
   describe "associations" do
-    it { should have_many(:employees) }
-    it { should have_many(:apartments) }
-    it { should have_many(:residents).through(:apartments) }
+    it { is_expected.to have_many(:employees) }
+    it { is_expected.to have_many(:apartments) }
+    it { is_expected.to have_many(:residents).through(:apartments) }
   end
 
   describe "validations" do
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:city) }
-    it { should validate_presence_of(:state) }
-    it { should validate_presence_of(:address) }
-    it { should validate_presence_of(:number) }
-    it { should validate_presence_of(:zipcode) }
-    it { should validate_presence_of(:neighborhood) }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:city) }
+    it { is_expected.to validate_presence_of(:state) }
+    it { is_expected.to validate_presence_of(:address) }
+    it { is_expected.to validate_presence_of(:number) }
+    it { is_expected.to validate_presence_of(:zipcode) }
+    it { is_expected.to validate_presence_of(:neighborhood) }
 
-    it { should validate_uniqueness_of(:name).scoped_to(:city, :address) }
+    it { is_expected.to validate_uniqueness_of(:name).scoped_to(:city, :address) }
   end
 end
