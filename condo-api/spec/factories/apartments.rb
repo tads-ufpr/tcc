769d2b5d9417044e58f8eae92a153243d85dc172
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-11-14 14:29:34 -0300

    new factory to apartments starting with residents

--- condo-api/spec/factories/apartments.rb
+++ condo-api/spec/factories/apartments.rb
@@ -10,5 +10,23 @@ FactoryBot.define do
     trait :approved do
       status { 1 }
     end
+
+    trait :with_residents do
+      transient do
+        residents_count { 1 }
+        owner_exists { true }
+      end
+
+      after(:create) do |apartment, evaluator|
+        if evaluator.owner_exists
+          create(:resident, apartment: apartment, user: create(:user), owner: true)
+          (evaluator.residents_count - 1).times do
+            create(:resident, apartment: apartment, user: create(:user), owner: false)
+          end
+        else
+          create_list(:resident, evaluator.residents_count, apartment: apartment, owner: false)
+        end
+      end
+    end
   end
 end
