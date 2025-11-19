75bb7e8061b523c4d4a234bfa497d5e52962eb28
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-11-19 18:01:49 -0300

    add new route to get users permissions

--- condo-api/config/routes.rb
+++ condo-api/config/routes.rb
@@ -1,27 +1,32 @@
 Rails.application.routes.draw do
-  devise_for :users, path: "user", path_names: {
-    sign_in: "login",
-    sign_out: "logout"
-  },
-  controllers: {
-    sessions: "users/sessions",
-    registrations: "users/registrations",
-    passwords: "users/passwords"
-  }
+  defaults format: :json do
+    devise_for :users, path: "user", path_names: {
+      sign_in: "login",
+      sign_out: "logout"
+    },
+    controllers: {
+      sessions: "users/sessions",
+      registrations: "users/registrations",
+      passwords: "users/passwords"
+    }
 
-  resources :condominia do
-    resources :notices, only: [:index]
-    resources :employees, only: [:index, :create]
-    resources :apartments, only: [:index, :create]
-  end
-  resources :apartments, only: [:show, :update, :destroy] do
-    patch :approve, on: :member
-    resources :notices, only: [:index, :create]
-    resources :residents, only: [:create, :update, :destroy]
-  end
-  resources :notices, only: [:show, :update, :destroy]
-  resources :users, only: [:show, :update, :create]
-  resources :employees, only: [:show, :update, :destroy]
+    namespace :users do
+      get "me/permissions", to: "permissions#show"
+    end
 
+    resources :condominia do
+      resources :notices, only: [:index]
+      resources :employees, only: [:index, :create]
+      resources :apartments, only: [:index, :create]
+    end
+    resources :apartments, only: [:show, :update, :destroy] do
+      patch :approve, on: :member
+      resources :notices, only: [:index, :create]
+      resources :residents, only: [:create, :update, :destroy]
+    end
+    resources :notices, only: [:show, :update, :destroy]
+    resources :users, only: [:show, :update, :create]
+    resources :employees, only: [:show, :update, :destroy]
+  end
   get "up" => "rails/health#show", as: :rails_health_check
 end
