4892899e81d443870edc9e8019e387c3ca852b64
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-11-30 22:46:14 -0300

    add tests to facility's model

--- condo-api/spec/models/facility_spec.rb
+++ condo-api/spec/models/facility_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Facility, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:condominium) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:tax) }
+    it { should validate_numericality_of(:tax).only_integer.is_greater_than_or_equal_to(0) }
+  end
 end
