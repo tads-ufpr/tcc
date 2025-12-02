a18c945fd9823cee8d4622e5439b7a5bf4482a24
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-12-02 16:28:44 -0300

    removed tests validating the :tax and :description presence

--- condo-api/spec/models/facility_spec.rb
+++ condo-api/spec/models/facility_spec.rb
@@ -2,13 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Facility, type: :model do
   describe 'associations' do
-    it { should belong_to(:condominium) }
+    it { is_expected.to belong_to(:condominium) }
   end
 
   describe 'validations' do
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:description) }
-    it { should validate_presence_of(:tax) }
-    it { should validate_numericality_of(:tax).only_integer.is_greater_than_or_equal_to(0) }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_numericality_of(:tax).only_integer.is_greater_than_or_equal_to(0) }
   end
 end
