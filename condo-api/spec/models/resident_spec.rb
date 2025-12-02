a45a3fe33b5a296093afd1402ddd4d20a2db07ae
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-12-02 18:13:15 -0300

    add residents model specs

--- condo-api/spec/models/resident_spec.rb
+++ condo-api/spec/models/resident_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Resident, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:apartment) }
+  end
+
+  describe 'validations' do
+    subject { create(:resident) }
+
+    it { is_expected.to validate_uniqueness_of(:user_id).scoped_to(:apartment_id) }
+
+    describe '#first_resident?' do
+      let(:apartment) { create(:apartment) }
+
+      context 'when it is the first resident' do
+        let(:resident) { create(:resident, apartment: apartment) }
+
+        it 'sets the resident as owner' do
+          expect(resident.owner).to be(true)
+        end
+      end
+
+      context 'when it is not the first resident' do
+        before { create(:resident, apartment: apartment, owner: true) }
+
+        let(:new_resident) { create(:resident, apartment: apartment) }
+
+        it 'does not set the new resident as owner' do
+          expect(new_resident.owner).to be(false)
+        end
+      end
+    end
+  end
 end
