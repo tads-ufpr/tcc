180232ae62277060545694a9480ff8b6b4d79880
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-12-02 16:18:19 -0300

    add business logic to Reservation and validating it on spec/models

--- condo-api/app/models/reservation.rb
+++ condo-api/app/models/reservation.rb
@@ -2,7 +2,34 @@ class Reservation < ApplicationRecord
   belongs_to :facility
   belongs_to :apartment
   belongs_to :creator, class_name: 'User'
+
+  validates :apartment, presence: true
   validate :creator_must_be_resident_of_apartment, on: :create
+
+  validate :scheduled_date_is_valid, on: :create
+  validate :apartment_pending_reservations_limit, on: :create
+
+  private
+
+  def scheduled_date_is_valid
+    return if scheduled_date.blank?
+
+    if scheduled_date < Date.today
+      errors.add(:scheduled_date, "can't be in the past")
+    end
+
+    if scheduled_date > Date.today + 2.months
+      errors.add(:scheduled_date, "can't be more than 2 months in the future")
+    end
+  end
+
+  def apartment_pending_reservations_limit
+    return if apartment.blank?
+
+    if apartment.reservations.where('scheduled_date >= ?', Date.today).count >= 2
+      errors.add(:apartment, 'has reached the limit of pending reservations')
+    end
+  end
 
   def creator_must_be_resident_of_apartment
     return if apartment.blank? || creator.blank?
--- condo-api/spec/models/reservation_spec.rb
+++ condo-api/spec/models/reservation_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:facility) }
+    it { should belong_to(:apartment) }
+    it { should belong_to(:creator).class_name('User') }
+  end
+
+  describe 'validations' do
+    let(:apartment) { create(:apartment) }
+
+    describe 'apartment pending reservations limit' do
+      let!(:reservation1) { create(:reservation, apartment: apartment, scheduled_date: 1.day.from_now) }
+      let!(:reservation2) { create(:reservation, apartment: apartment, scheduled_date: 2.days.from_now) }
+
+      it 'does not allow a third pending reservation for the same apartment' do
+        reservation3 = build(:reservation, apartment: apartment, scheduled_date: 3.days.from_now)
+        expect(reservation3).not_to be_valid
+        expect(reservation3.errors[:apartment]).to include('has reached the limit of pending reservations')
+      end
+
+      it 'allows a third reservation if one is in the past' do
+        reservation1.update!(scheduled_date: 1.day.ago)
+        reservation3 = build(:reservation, apartment: apartment, scheduled_date: 3.days.from_now)
+        expect(reservation3).to be_valid
+      end
+    end
+
+    describe 'scheduled_date' do
+      it 'cannot be in the past' do
+        reservation = build(:reservation, apartment: apartment, scheduled_date: 1.day.ago)
+        expect(reservation).not_to be_valid
+        expect(reservation.errors[:scheduled_date]).to include("can't be in the past")
+      end
+
+      it 'cannot be more than 2 months in the future' do
+        reservation = build(:reservation, apartment: apartment, scheduled_date: 2.months.from_now + 1.day)
+        expect(reservation).not_to be_valid
+        expect(reservation.errors[:scheduled_date]).to include("can't be more than 2 months in the future")
+      end
+
+      it 'can be today' do
+        reservation = build(:reservation, apartment: apartment, scheduled_date: Date.today)
+        expect(reservation).to be_valid
+      end
+
+      it 'can be 2 months in the future' do
+        reservation = build(:reservation, apartment: apartment, scheduled_date: 2.months.from_now)
+        expect(reservation).to be_valid
+      end
+    end
+  end
 end
