866b4c75543ec64871be9ce38bdd135e9f759464
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-12-11 14:15:01 -0300

    allowing ngrok as hosting

--- condo-api/config/environments/development.rb
+++ condo-api/config/environments/development.rb
@@ -38,6 +38,10 @@ Rails.application.configure do
   # Set localhost to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
+  # Allow ngrok to access the development environment
+  config.hosts << ".ngrok-free.app"
+
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
